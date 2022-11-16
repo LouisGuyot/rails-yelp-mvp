d9dfd0569314daf243052dd3017448ce4bc2e8a9
Louis Guyot <louis.guyot.pro@gmail.com>
2022-11-16 14:41:15 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'restaurants#index'
+
+  resources :restaurants, only: [:index, :new, :show, :create]
+  resources :restaurants do
+    resources :reviews, only: [:show, :new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+Restaurant.create!(name: 'De Clercq', address: '169 Rue Montmartre, 75002 Paris', phone_number: '01 42 21 49 57', category: 'belgian')
+Restaurant.create!(name: 'Pizza East', address: '56A Shoreditch High St', phone_number: '+44 20 7729 1888', category: 'italian')
+Restaurant.create!(name: 'Pizza West', address: '56B Shoreditch High St', phone_number: '+44 20 7729 1889', category: 'italian')
+Restaurant.create!(name: 'Tonkatsu Tombo', address: "14 Rue de l'Arrivée", phone_number:' 01 42 22 61 83', category: 'japanese')
+Restaurant.create!(name: 'Gaya', address: '6 Rue de Saint-Simon', phone_number: '01 45 44 73 73', category: 'french')
+
+
+puts 'Finished!'
